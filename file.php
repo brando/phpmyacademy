7311a7d351e70de839131286d910563cac454cb9
Matúš Brandýs <matus.brandys@ynet.sk>
2014-12-22 14:46:22 +0100

    praca so subormi (upload), snazil som sa aj zabezpecit co do najvatsiej miery; ale este by sa dalo obmedzit velkost vkladaneho suboru

---
+++ file.php
@@ -0,0 +1,46 @@
+<?php
+/*
+*$nazov=$_FILES['file']['name'];
+*$velkost=$_FILES['file']['size'];
+*$typ_suboru=$_FILES['file']['type'];
+*$doc_miesto=$_FILES['file']['tmp_name'];
+*nemozem tu definovat lebo mi vyhodi undefined index lebo musim overit pomocou isset() funkcie
+*/
+
+if(isset($_FILES['file']['name']))
+{
+$nazov=$_FILES['file']['name'];
+$velkost=$_FILES['file']['size'];
+echo $type=$_FILES['file']['type'];	//vrati image/jpeg=>pri obrazkoch ; text/plain pri textovom subole atd...
+$doc_miesto=$_FILES['file']['tmp_name'];
+
+$real_location='uploads/';
+$typ_suboru=strtolower(substr($nazov,strpos($nazov,'.')+1));
+	if(!empty($nazov))
+	{
+		if($type=='image/jpeg' && ($typ_suboru=='jpeg' || $typ_suboru=='jpg'))
+		{
+			if(file_exists($real_location.$nazov))				//nazov suboru =>samozrejem ja to ukladam do ineho directory tak preto je tam aj premena $real_location
+			{
+			echo 'nazov tohto suboru uz existuje prosim premenuj!!!';
+			}
+			else 
+			{
+				if(move_uploaded_file($doc_miesto,$real_location.$nazov))
+				echo 'Ulozene!!';
+				else
+				echo 'error pri ukladanii';
+			}
+		}
+		else
+		echo 'povolene vkaldat iba obrazku typu jpg a jpeg';
+	}
+	else
+	echo 'prosim vyber subor ';
+}
+?>
+
+<form action="<?php echo $_SERVER['SCRIPT_NAME']?>" method="POST" enctype="multipart/form-data">
+	<input type="file" name="file"><br><br>
+	<input type="submit" value="vloz subor">
+</form>
