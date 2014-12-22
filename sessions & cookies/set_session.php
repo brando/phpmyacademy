87398b761938839f3dfc82db4708968b3602c539
Matúš Brandýs <matus.brandys@ynet.sk>
2014-12-22 12:40:08 +0100

    praca so session setting start unseting

---
+++ sessions & cookies/set_session.php
@@ -0,0 +1,8 @@
+<?php
+//ob_start();
+session_start();
+$_SESSION['name']='Matus';
+//header("Location:http://localhost/phpmyacademy/sessions%20&%20cookies/view.php");
+//ob_end_flush();
+?>
+<a href="http://localhost/phpmyacademy/sessions%20&%20cookies/view.php">presmerovanie na view</a>
